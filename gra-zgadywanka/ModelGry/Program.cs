6c66312ab6dda4d66b946efe9714258b26e11456
Gorzelany Artur <artur.gorzelany@microsoft.wsei.edu.pl>
2019-05-19 10:15:18 +0200

    testing

---
+++ gra-zgadywanka/ModelGry/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelGry
+{
+    public class Gra
+    {
+
+        int zakresOd;
+        int zakresDo;
+        int wylosowana;
+
+        public Gra(int min = 1, int max = 100)
+        {
+            zakresOd = min;
+            zakresDo = max;
+            wylosowana = Losuj(min, max);
+        }
+
+        private int Losuj(int min = 1, int max = 100) //wartosci domyslne
+        {
+            if (min > max)
+            {
+                int temp = max; //zmienna dodatkowa 
+                max = min;
+                min = temp;
+            }
+            Random generator = new Random();
+            return generator.Next(min, max + 1);
+        }
+        static void Main(string[] args)
+        {
+            
+           
+
+        }
+    }
+}
---
+++ gra-zgadywanka/UnitTestProjectModelGry/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelGry;
+
+namespace UnitTestProjectModelGry
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Losuj_OK()
+        {
+            //arrange-act-assert
+
+            //arrange
+            int min = 1;
+            int max = 10;
+            //act
+            int wylos = Gra.Losuj(min, max);
+
+            //Assert
+            Assert.IsTrue(wylos >= min && wylos <= max);
+        }
+    }
+}
